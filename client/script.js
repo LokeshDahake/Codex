82a4a9e7f26b0f1655f3f5114664f411078bd3be
20BD1A05AE <20bd1a05aecsef@gmail.com>
2023-04-11 21:48:37 +0530

    update the server url

--- client/script.js
+++ client/script.js
@@ -75,7 +75,7 @@ const handleSubmit = async (e) => {
 
   loader(messageDiv);
 
-  const response = await fetch('https://code-ai-vd7q.onrender.com',{
+  const response = await fetch('https://code-ai-vd7q.onrender.com/',{
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
